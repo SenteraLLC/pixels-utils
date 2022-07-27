b79d3c76d9c1b1dfed7ebf326accbce590dde6cd
tnigon <8249296+tnigon@users.noreply.github.com>
2022-07-26 19:22:28 -0500

    adding constants

---
+++ pixels_utils/constants/sentinel2.py
@@ -0,0 +1,61 @@
+from enum import IntEnum
+
+ELEMENT84_SEARCH_URL = "https://earth-search.aws.element84.com/v0/search"
+ELEMENT84_L2A_SCENE_URL = (
+    "https://earth-search.aws.element84.com/v0/"
+    "collections/{collection}/items/{sceneid}"
+)
+
+SENTINEL_2_L2A_COLLECTION = "sentinel-s2-l2a-cogs"
+SENTINEL_2_L1C_COLLECTION = "sentinel-s2-l1c-cogs"
+
+
+ASSETS_TCI = ("visual",)
+ASSETS_MSI = (
+    "B02",
+    "B03",
+    "B04",
+    "B05",
+    "B06",
+    "B07",
+    "B08",
+    "B8A",
+    "B09",
+    "B11",
+    "B12",
+)
+ASSETS_SPECIAL = ("AOT", "SCL", "WVP")
+SENTINEL2_UKIS = ("B02", "B03", "B04", "B08", "B11", "B12")
+
+EXPRESSION_NDVI = "(B08-B04)/(B08+B04)"
+EXPRESSION_NDRE = "(B08-B05)/(B08+B05)"
+
+
+class SCL(IntEnum):
+    """Sentinel-2 L2A Scene Classification classes.
+
+    From https://sentinels.copernicus.eu/web/sentinel/technical-guides/sentinel-2-msi/level-2a/algorithm
+    """
+
+    NO_DATA = 0
+    SATURATED_OR_DEFECTIVE = 1
+    CAST_SHADOWS = 2
+    CLOUD_SHADOWS = 3
+    VEGETATION = 4
+    BARE_SOIL = 5
+    WATER = 6
+    UNCLASSIFIED = 7
+    CLOUD_MEDIUM_PROBABILITY = 8
+    CLOUD_HIGH_PROBABILITY = 9
+    THIN_CIRRUS = 10
+    SNOW_OR_ICE = 11
+
+
+SCL_GROUP_ARABLE = [SCL.VEGETATION, SCL.BARE_SOIL]
+SCL_GROUP_CLOUDS = [
+    SCL.CAST_SHADOWS,
+    SCL.CLOUD_SHADOWS,
+    SCL.CLOUD_MEDIUM_PROBABILITY,
+    SCL.CLOUD_HIGH_PROBABILITY,
+    SCL.THIN_CIRRUS,
+]
---
+++ pixels_utils/constants/titiler.py
@@ -0,0 +1,10 @@
+PIXELS_URL = "https://pixels.sentera.com/stac/{endpoint}"
+
+ENDPOINT_INFO = "info"
+ENDPOINT_STATISTICS = "statistics"
+
+NODATA_STR = "nodata_value"
+
+QUERY_URL = "url"
+QUERY_ASSETS = "assets"
+QUERY_EXPRESSION = "expression"
