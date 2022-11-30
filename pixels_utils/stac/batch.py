c98206f6811165a0d8db3a34820349d6127b4188
tnigon <8249296+tnigon@users.noreply.github.com>
2022-11-30 11:09:14 -0600

    succesful batch.crop with metadata!

--- pixels_utils/stac/batch.py
+++ pixels_utils/stac/batch.py
@@ -1,20 +1,23 @@
 import logging
 from datetime import date, datetime
+from pathlib import Path
 from typing import Any, Dict, Iterable, List, Tuple, Union
 
 import pytz
-from geo_utils.validate import ensure_valid_featurecollection, get_all_geojson_geometries
+from geo_utils.validate import ensure_valid_feature, ensure_valid_featurecollection, get_all_geojson_geometries
 from numpy import ndarray as np_ndarray
+from numpy.typing import ArrayLike
 from pandas import DataFrame
 from pandas import concat as pd_concat
+from pyproj import CRS
 from retry import retry
 
+from pixels_utils import __version__ as PIXELS_UTILS_VERSION  # noqa
 from pixels_utils.constants.decorators import requires_rasterio
 from pixels_utils.constants.sentinel2 import ELEMENT84_L2A_SCENE_URL, SCL, SENTINEL_2_L2A_COLLECTION
-from pixels_utils.constants.types import STAC_crop
 from pixels_utils.scenes import bbox_from_geometry, get_stac_scenes
-from pixels_utils.stac.endpoint import statistics_response
-from pixels_utils.utils_crop import response_to_rasterio
+from pixels_utils.stac.endpoint import crop_response, statistics_response
+from pixels_utils.utils_crop import count_geojson_pixels, count_valid_whitelist_pixels
 from pixels_utils.utils_statistics import (
     combine_stats_and_meta_dicts,
     compute_whitelist_stats,
@@ -23,50 +26,50 @@ from pixels_utils.utils_statistics import (
 )
 
 try:
-    from rasterio import Env
+    from geo_utils.raster import save_rasterio
     from rasterio.profiles import Profile
 
-    from pixels_utils.rasterio_helper import combine_profile_tags
+    from pixels_utils.utils_crop import parse_crop_response
 except ImportError:
     logging.exception("Optional dependency <rasterio> not imported. Some features are not available.")
 
 
-@retry(KeyError, tries=3, delay=2)
-def scl_stats(
-    scene_url: str,
-    geojson: Any,
-    whitelist: bool = True,
-    nodata: Union[int, float] = None,
-    gsd: Union[int, float] = 20,
-    resampling: str = "nearest",
-    acquisition_time: str = None,
-    cloud_cover_scene_pct: float = None,
-    clear_cache_iter=iter([False, True, True]),
-) -> tuple[Dict, Dict]:
-    geojson_fc = ensure_valid_featurecollection(geojson, create_new=False)
-    stats_kwargs = {
-        "scene_url": scene_url,
-        "assets": "SCL",
-        "expression": None,
-        "geojson": geojson_fc,
-        "mask_scl": None,
-        "whitelist": whitelist,
-        "nodata": nodata,
-        "gsd": gsd,
-        "resampling": resampling,
-        "categorical": True,
-        "c": list(range(12)),
-        "histogram_bins": None,
-    }
+# @retry(KeyError, tries=3, delay=2)
+# def scl_stats(
+#     scene_url: str,
+#     geojson: Any,
+#     whitelist: bool = True,
+#     nodata: Union[int, float] = None,
+#     gsd: Union[int, float] = 20,
+#     resampling: str = "nearest",
+#     acquisition_time: str = None,
+#     cloud_cover_scene_pct: float = None,
+#     clear_cache_iter=iter([False, True, True]),
+# ) -> tuple[Dict, Dict]:
+#     geojson_fc = ensure_valid_featurecollection(geojson, create_new=False)
+#     stats_kwargs = {
+#         "scene_url": scene_url,
+#         "assets": "SCL",
+#         "expression": None,
+#         "geojson": geojson_fc,
+#         "mask_scl": None,
+#         "whitelist": whitelist,
+#         "nodata": nodata,
+#         "gsd": gsd,
+#         "resampling": resampling,
+#         "categorical": True,
+#         "c": list(range(12)),
+#         "histogram_bins": None,
+#     }
 
-    scene_dict = {
-        "request_time_scl": (datetime.now().astimezone(pytz.utc).strftime("%Y-%m-%dT%H:%M:%S%z")),
-        "acquisition_time": acquisition_time,
-        "cloud_cover_scene_pct": cloud_cover_scene_pct,
-    }
-    r_scl = statistics_response(**stats_kwargs, clear_cache=next(clear_cache_iter))
-    stats_dict_scl, meta_dict_scl = parse_stats_response(r_scl, **scene_dict, **stats_kwargs)
-    return stats_dict_scl, meta_dict_scl
+#     scene_dict = {
+#         "request_time_scl": (datetime.now().astimezone(pytz.utc).strftime("%Y-%m-%dT%H:%M:%S%z")),
+#         "acquisition_time": acquisition_time,
+#         "cloud_cover_scene_pct": cloud_cover_scene_pct,
+#     }
+#     r_scl = statistics_response(**stats_kwargs, clear_cache=next(clear_cache_iter))
+#     stats_dict_scl, meta_dict_scl = parse_stats_response(r_scl, **scene_dict, **stats_kwargs)
+#     return stats_dict_scl, meta_dict_scl
 
 
 def statistics(
@@ -83,9 +86,11 @@ def statistics(
     resampling: str = "nearest",
     categorical: bool = False,
     c: List[Union[float, int]] = None,
+    p: List[int] = None,
     histogram_bins: str = None,
+    histogram_range: ArrayLike = None,
 ) -> DataFrame:
-    geojson_fc = ensure_valid_featurecollection(geojson, create_new=True)
+    geojson_fc = None if geojson is None else ensure_valid_featurecollection(geojson, create_new=True)
     # in this case, <next> returns only the first geometry.
     # TODO: Should there be a warning or exception raised if there happen to be multiple geometries passed?
     df_scenes = get_stac_scenes(
@@ -105,7 +110,6 @@ def statistics(
         "assets": assets,
         "expression": expression,
         "geojson": geojson_fc,
-        # "geojson": geojson_fc.wkt,
         "mask_scl": mask_scl,
         "whitelist": whitelist,
         "nodata": nodata,
@@ -113,7 +117,9 @@ def statistics(
         "resampling": resampling,
         "categorical": categorical,
         "c": c,
+        "p": p,
         "histogram_bins": histogram_bins,
+        "histogram_range": histogram_range,
     }
     df_stats = None
     for i, scene in df_scenes.iterrows():
@@ -126,6 +132,7 @@ def statistics(
         @retry(KeyError, tries=3, delay=2)
         def run_stats(
             acquisition_time: str = None,
+            stats_kwargs: Dict = None,
             cloud_cover_scene_pct: float = None,
             clear_cache_iter=iter([False, True, True]),
         ):
@@ -143,6 +150,8 @@ def statistics(
                 clear_cache=next(clear_cache_iter),  # Clears cache after 1st try in case error is raised
             )
 
+            stats_kwargs = {k: v for k, v in stats_kwargs.items()}
+            stats_kwargs["version"] = PIXELS_UTILS_VERSION
             (stats_dict, meta_dict,) = parse_stats_response(  # Must be in same function as statistics_response() call
                 r, **scene_dict, **stats_kwargs
             )
@@ -153,20 +162,33 @@ def statistics(
         try:
             stats_dict, meta_dict = run_stats(
                 acquisition_time,
+                stats_kwargs,
                 cloud_cover_scene_pct,
                 clear_cache_iter=iter([False, True, True]),
             )
-            stats_dict_scl, meta_dict_scl = scl_stats(
-                stats_kwargs["scene_url"],
-                geojson_fc,
-                whitelist,
-                nodata,
-                gsd,
-                resampling,
-                acquisition_time=acquisition_time,
-                cloud_cover_scene_pct=cloud_cover_scene_pct,
+
+            stats_kwargs_scl = {
+                k: v for k, v in stats_kwargs.items() if k not in ["assets", "expression", "mask_scl", "whitelist"]
+            }
+            stats_kwargs_scl["expression"] = "SCL"
+            stats_dict_scl, meta_dict_scl = run_stats(
+                acquisition_time,
+                stats_kwargs,
+                cloud_cover_scene_pct,
                 clear_cache_iter=iter([False, True, True]),
             )
+
+            # stats_dict_scl, meta_dict_scl = scl_stats(
+            #     stats_kwargs["scene_url"],
+            #     geojson_fc,
+            #     whitelist,
+            #     nodata,
+            #     gsd,
+            #     resampling,
+            #     acquisition_time=acquisition_time,
+            #     cloud_cover_scene_pct=cloud_cover_scene_pct,
+            #     clear_cache_iter=iter([False, True, True]),
+            # )
             (
                 whitelist_pixels,
                 whitelist_pct,
@@ -217,17 +239,173 @@ def statistics(
 
 
 @requires_rasterio
-def crop(r: STAC_crop) -> Tuple[np_ndarray, Profile]:
-    """
-    Eqivalent to:
-        >>> r = crop_response(...)
-        >>> ds = response_to_rasterio(...)
-        >>> ds.data, ds.profile
-    """
-    with Env(), response_to_rasterio(r) as ds:
-        # data = ds.read()
-        data = ds.read(masked=False)
-        profile = combine_profile_tags(ds)
+def crop(
+    date_start: Union[date, str],
+    date_end: Union[date, str],
+    geojson: Any = None,
+    collection: str = SENTINEL_2_L2A_COLLECTION,
+    assets: Iterable[str] = None,
+    expression: str = None,
+    mask_scl: Iterable[SCL] = None,
+    whitelist: bool = True,
+    nodata: Union[int, float] = None,
+    gsd: Union[int, float] = 20,
+    resampling: str = "nearest",
+    unscale: Union[bool, None] = None,
+    rescale: ArrayLike = None,
+    color_formula: Union[str, None] = None,
+    colormap: Union[Dict, None] = None,
+    colormap_name: Union[str, None] = None,
+    return_mask: Union[bool, None] = None,
+    format_stac: Union[str, None] = ".tif",
+    crs: CRS = CRS.from_epsg(4326),
+    dir_out: Path = None,
+    minimum_whitelist: Union[int, float] = None,
+    max_images: Union[int, float] = None,
+) -> Tuple[np_ndarray, Profile]:
+    geojson_f = None if geojson is None else ensure_valid_feature(geojson, create_new=False)
+    # in this case, <next> returns only the first geometry.
+    # TODO: Should there be a warning or exception raised if there happen to be multiple geometries passed?
+    df_scenes = get_stac_scenes(
+        bbox_from_geometry(next(get_all_geojson_geometries(geojson_f))),
+        date_start,
+        date_end,
+    )
+    # df_scenes = get_stac_scenes(bbox_from_geometry(geojson_fc), date_start, date_end)
+    if len(df_scenes) > 0:
+        logging.info("Getting STAC crop rasters. Number of scenes: %s", len(df_scenes))
+    else:
+        logging.warning("No valid scenes for AOI from %s to %s.", date_start, date_end)
+        return
 
-    # data_out, profile_out = geoml_metadata(data, profile)
-    return data, profile
+    crop_kwargs = {
+        "scene_url": None,
+        "assets": assets,
+        "expression": expression,
+        "geojson": geojson_f,
+        "mask_scl": mask_scl,
+        "whitelist": whitelist,
+        "nodata": nodata,
+        "gsd": gsd,
+        "resampling": resampling,
+        "unscale": unscale,
+        "rescale": rescale,
+        "color_formula": color_formula,
+        "colormap": colormap,
+        "colormap_name": colormap_name,
+        "return_mask": return_mask,
+        "format_stac": format_stac,
+    }
+
+    img_count = 0
+    for i, scene in df_scenes.iterrows():
+        # if i > -1:
+        #     break
+        crop_kwargs["scene_url"] = ELEMENT84_L2A_SCENE_URL.format(collection=collection, sceneid=scene["id"])
+        acquisition_time = scene["datetime"]
+        cloud_cover_scene_pct = scene["eo:cloud_cover"]
+
+        @retry(KeyError, tries=3, delay=2)
+        def run_crop(
+            acquisition_time: str = None,
+            crop_kwargs: Dict = None,
+            cloud_cover_scene_pct: float = None,
+            clear_cache_iter=iter([False, True, True]),
+        ):
+            """Runs crop_response() and count_geojson_pixels() together so if KeyError it can retry."""
+            scene_tags = {
+                "request_time": (datetime.now().astimezone(pytz.utc).strftime("%Y-%m-%dT%H:%M:%S%z")),
+                "acquisition_time": acquisition_time,
+                "cloud_cover_scene_pct": cloud_cover_scene_pct,
+            }
+            r = crop_response(
+                **crop_kwargs,
+                clear_cache=next(clear_cache_iter),  # Clears cache after 1st try in case error is raised
+            )
+
+            pixels_params = {k: v for k, v in crop_kwargs.items()}
+            if "mask_scl" in pixels_params:
+                pixels_params["mask_scl"] = [i.name for i in pixels_params["mask_scl"]]
+            pixels_params["version"] = PIXELS_UTILS_VERSION
+            data, profile, tags = parse_crop_response(  # Must be in same function as crop_response() call
+                r, **scene_tags, pixels_params=pixels_params
+            )
+            # Update profile with stats
+            band_names = (
+                (pixels_params["expression"],)
+                if isinstance(pixels_params["expression"], str)
+                else pixels_params["expression"]
+            )
+            tags["geojson_stats"] = count_geojson_pixels(data, band_names)
+            return data, profile, tags
+
+        # TODO: A separate nested function that only runs if r has an error
+        try:
+            data, profile, tags = run_crop(
+                acquisition_time,
+                crop_kwargs,
+                cloud_cover_scene_pct,
+                clear_cache_iter=iter([True, True, True]),
+            )
+            if crop_kwargs["mask_scl"]:  # only get SCL if first request had masking by SCL performed
+                crop_kwargs_scl = {
+                    k: v for k, v in crop_kwargs.items() if k not in ["assets", "expression", "mask_scl", "whitelist"]
+                }
+                crop_kwargs_scl["expression"] = "SCL"
+                # crop_kwargs_scl["nodata"] = -1
+                data_scl, profile_scl, tags_scl = run_crop(
+                    acquisition_time,
+                    crop_kwargs_scl,
+                    cloud_cover_scene_pct,
+                    clear_cache_iter=iter([True, True, True]),
+                )
+                tags["mask_scl_stats"] = count_valid_whitelist_pixels(data_scl, mask_scl, whitelist)
+                tags["request_time_scl"] = tags_scl["request_time"]
+            else:
+                tags["mask_scl_stats"] = None
+                tags["request_time_scl"] = None
+            logging.info("Retrieving scene %s/%s: SUCCESS", i + 1, len(df_scenes))
+        except TypeError:  # Fill in what we can so program can continue for other scenes in date range.
+            data, profile, tags = None, None, None
+            logging.warning("Retrieving scene  %s/%s: TypeError", i + 1, len(df_scenes))
+        except KeyError:
+            data, profile, tags = None, None, None
+            logging.warning("Retrieving scene %s/%s: KeyError", i + 1, len(df_scenes))
+
+        logging.debug("Scene URL: %s", crop_kwargs["scene_url"])
+        if data is None or profile is None:
+            continue
+        if tags["mask_scl_stats"]["whitelist_pct"] >= minimum_whitelist:
+            # data_out, profile_out = geoml_metadata(data, profile)
+            # geojson_hash = int(hashlib.sha256(dumps(geojson).encode("utf-8")).hexdigest(), 16) % 10**6
+            # fname_out = "_".join([scene["id"], "sha256-" + str(geojson_hash)]) + format_stac
+            fname_out = "_".join([scene["id"], f"{dir_out.parts[-1]}"]) + format_stac
+            band_names = (
+                (crop_kwargs["expression"],)
+                if isinstance(crop_kwargs["expression"], str)
+                else crop_kwargs["expression"]
+            )
+
+            save_rasterio(
+                dir_out=dir_out,
+                fname_out=fname_out,
+                arr=data,
+                profile=profile,
+                tags=tags,
+                band_description=band_names,
+                crs=crs,
+            )
+            img_count += 1
+        if img_count >= max_images:
+            logging.info("Maximum allowable image count (%s) is met. Stopping.", max_images)
+            break
+
+    # with WarpedVRT(ds, crs=crs) as vrt:
+    #     ds_warp
+    # with rasterio.open('tests/data/RGB.byte.tif') as src:
+    # with WarpedVRT(src, crs=crs) as vrt:
+    #     data = vrt.read()
+    #     # TODO: Either return vrt (?) or save vrt.data/vrt.profile to disk
+
+    # # data_out, profile_out = geoml_metadata(data, profile)
+    # return data, profile
--- pixels_utils/utils_crop.py
+++ pixels_utils/utils_crop.py
@@ -1,24 +1,129 @@
 import logging
-from typing import Union
+from typing import Dict, Iterable, Tuple, Union
 
 import numpy.ma as ma
+from numpy import count_nonzero as np_count_nonzero
 from numpy import expand_dims as np_expand_dims
+from numpy import isin as np_isin
 from numpy import logical_not as np_logical_not
 from numpy import min_scalar_type as np_min_scalar_type
 from numpy import ndarray as np_ndarray
 from numpy import repeat as np_repeat
+from numpy.ma import count as npma_count
+from numpy.ma import count_masked as npma_count_masked
 from numpy.typing import ArrayLike
 
 from pixels_utils.constants.decorators import requires_rasterio
+from pixels_utils.constants.sentinel2 import SCL
 from pixels_utils.constants.types import STAC_crop
 
 try:
+    from rasterio import Env
     from rasterio.io import DatasetReader, MemoryFile
     from rasterio.profiles import Profile
 
     from pixels_utils.rasterio_helper import ensure_data_profile_consistency, save_image
 except ImportError:
     logging.exception("Optional dependency <rasterio> not imported. Some features are not available.")
+
+
+# A_: Total within and total outside geojson
+A1 = "geojson_in_pix"
+A2 = "geojson_out_pix"
+A3 = "geojson_in_pct"
+A4 = "geojson_out_pct"
+# B_: Total unmasked (valid) and masked (invalid) within geojson
+B1 = "whitelist_pix"
+B2 = "blacklist_pix"
+B3 = "whitelist_pct"
+B4 = "blacklist_pct"
+# C_: Breakdown of SCL classes within geojson
+C1 = "pix_by_scl"
+C2 = "pct_by_scl"
+
+
+def count_geojson_pixels(data: ArrayLike, band_names: Iterable) -> Dict:
+    """
+    Counts valid and invalid pixels for each band in data array.
+
+    Args:
+        data (ArrayLike): Data array. Must be 3-dimensional.
+        band_names (Iterable): Band names. These will be the keys of the returned dict.
+
+    Returns:
+        Dict: Geojson pixel stats within and outside geojson.
+    """
+    geojson_stats = {}
+    geojson_stats[A1] = {band_name: npma_count(data[i, :, :]) for i, band_name in enumerate(band_names)}
+    geojson_stats[A2] = {band_name: npma_count_masked(data[i, :, :]) for i, band_name in enumerate(band_names)}
+    # geojson_stats["valid_pix"] = {band_name: count(data[i, :, :]) - count_masked(data[i, :, :]) for i, band_name in enumerate(band_names)}
+    geojson_stats[A3] = {
+        bn1: v_pix / (v_pix + iv_pix) * 100
+        for (bn1, v_pix), (_, iv_pix) in zip(geojson_stats[A1].items(), geojson_stats[A2].items())
+    }
+    geojson_stats[A4] = {
+        bn1: iv_pix / (v_pix + iv_pix) * 100
+        for (bn1, v_pix), (_, iv_pix) in zip(geojson_stats[A1].items(), geojson_stats[A2].items())
+    }
+    return geojson_stats
+
+
+def count_valid_whitelist_pixels(data_scl: ArrayLike, mask_scl: Iterable, whitelist: bool) -> Dict:
+    """
+    Counts valid and invalid pixels in SCL array.
+
+    Args:
+        data_scl (ArrayLike): SCL array. Must be 3-dimensional.
+        mask_scl (Iterable): Sentinel-2 SCL classes to consider for pixel-based masking. If `None`,
+        pixel-based masking is not implemented. The `whitelist` setting must be considered to determine if `mask_scl`
+        classes are deemed to be valid or invalid.
+
+        whitelist (bool): If `True`, the passed `mask_scl` classes are considerd "valid" for pixel-based
+        masking; if `False`, the passed `mask_scl` classes are considered "invalid" for pixel-based masking.
+
+    Returns:
+        Dict: SCL pixel stats A) within and outside geojson, B) unmasked (valid) and masked (invalid) within geojson,
+        and breakdown of SCL classes within geojson.
+    """
+    mask_scl_stats = {}
+
+    # A_: Total within and total outside geojson
+    mask_scl_stats[A1] = npma_count(data_scl[0, :, :])  # Intersects geojson
+    mask_scl_stats[A2] = npma_count_masked(data_scl[0, :, :])  # Outside geojson
+    mask_scl_stats[A3] = mask_scl_stats[A1] / (mask_scl_stats[A1] + mask_scl_stats[A2]) * 100
+    mask_scl_stats[A4] = mask_scl_stats[A2] / (mask_scl_stats[A1] + mask_scl_stats[A2]) * 100
+
+    # B_: Total unmasked (valid) and masked (invalid) within geojson
+    if mask_scl and whitelist:
+        scl_wl = mask_scl
+    elif mask_scl and whitelist is False:
+        scl_wl = set(SCL) - set(mask_scl)
+    else:
+        scl_wl = None
+        mask_scl_stats[B1] = None
+        mask_scl_stats[B2] = None
+        mask_scl_stats[B3] = None
+        mask_scl_stats[B4] = None
+
+    if scl_wl:
+        mask_scl_stats[B1] = np_count_nonzero(
+            np_isin(data_scl.data[0, :, :], scl_wl)
+        )  # count of pixels in scl_wl (not including geojson mask)
+        mask_scl_stats[B2] = (
+            mask_scl_stats[A1] - mask_scl_stats[B1]
+        )  # count of pixels NOT in scl_wl (not including geojson mask)
+        # invalid_mask_scl_pix + valid_mask_scl_pix  # should equal sum(list(mask_scl_stats[C1].values())) - mask_scl_stats[C1]["GEOJSON_MASK"]
+        mask_scl_stats[B3] = mask_scl_stats[B1] / (mask_scl_stats[B1] + mask_scl_stats[B2]) * 100
+        mask_scl_stats[B4] = mask_scl_stats[B2] / (mask_scl_stats[B1] + mask_scl_stats[B2]) * 100
+
+    # C_: Breakdown of SCL classes within geojson
+    mask_scl_stats[C1] = {scl.name: np_count_nonzero(data_scl.data[0, :, :] == scl) for scl in SCL}
+    mask_scl_stats[C2] = {
+        scl.name: (np_count_nonzero(data_scl.data[0, :, :] == scl) / mask_scl_stats[A1]) * 100 for scl in SCL
+    }
+    mask_scl_stats[C1]["NO_DATA"] = mask_scl_stats[C1]["NO_DATA"] - mask_scl_stats[A2]
+    mask_scl_stats[C2]["NO_DATA"] = (mask_scl_stats[C1]["NO_DATA"] / mask_scl_stats[A1]) * 100
+    return mask_scl_stats
 
 
 def mask_stac_crop(data: ArrayLike, profile: Profile, nodata: Union[float, int] = 0) -> tuple[np_ndarray, Profile]:
@@ -39,6 +144,11 @@ def mask_stac_crop(data: ArrayLike, profile: Profile, nodata: Union[float, int] 
     # masked? Problem is that they both show up as "nodata". Better to probably get SCL specifically if/when it is
     # needed.
 
+    Options:
+        1. Get SCL separately before or at the time mask_stac_crop is called (messy)
+        2. Have separate function that uses geojson to calculate number and proportion of intersecting pixels per band.
+            - The difference between
+
     """
     nodata = 0 if nodata is None else nodata
 
@@ -55,6 +165,33 @@ def mask_stac_crop(data: ArrayLike, profile: Profile, nodata: Union[float, int] 
 
     array_mask, profile = ensure_data_profile_consistency(array_mask, profile, driver=profile["driver"])
     return array_mask, profile
+
+
+@requires_rasterio
+def parse_crop_response(r: STAC_crop, **kwargs) -> Tuple[ArrayLike, Profile]:
+    # data, profile = crop(r)  # batch.crop
+    if "nodata" in kwargs:
+        nodata = kwargs["nodata"]
+    else:
+        nodata = None
+
+    # ds = response_to_rasterio(r)
+    with Env(), response_to_rasterio(r) as ds:
+        # data = ds.read()
+        data = ds.read(masked=False)
+        profile = ds.profile
+        tags = ds.tags()
+        # print(ds.tags())
+        # print(ds.tag_namespaces())
+        # for ns in ds.tag_namespaces():
+        #     print(ds.tags(ns=ns))
+        #     tags.update(**ds.tags(ns=ns))
+        # profile = combine_profile_tags(ds)
+    data_mask, profile_mask = mask_stac_crop(data, profile, nodata=nodata)
+    tags.update(**profile_mask)
+    tags.update(**kwargs)
+    # profile_mask.update(**kwargs)
+    return data_mask, profile_mask, tags
 
 
 @requires_rasterio
