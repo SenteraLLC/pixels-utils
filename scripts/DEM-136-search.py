7c141a0c46baadaafe0fcc2c9c32767383c8ac4a
tnigon <8249296+tnigon@users.noreply.github.com>
2023-06-05 20:50:22 -0500

    clean up script

--- scripts/DEM-136-search.py
+++ scripts/DEM-136-search.py
@@ -15,7 +15,7 @@ geometry = Polygon((Point(6, 59), Point(-5, -2), Point(88, -46), Point(6, 59)))
 # %% Settings
 DATA_ID = 1
 OUTPUT_DIR = Path("/mnt/c/Users/Tyler/Downloads")
-STAC_VERSION = "v0"  # "v0" or "v1"
+STAC_VERSION = "v1"  # "v0" or "v1"
 
 geojson = sample_geojson(DATA_ID)
 # geojson_fc = ensure_valid_featurecollection(geojson, create_new=True)
@@ -35,8 +35,7 @@ elif STAC_VERSION == "v1":
     collection = EarthSearchCollections.sentinel_2_l2a
 # %% Run
 
-df_scenes_v0 = search_stac_scenes(
-    # geometry=Polygon((Point(6, 59), Point(-5, -2), Point(88, -46), Point(6, 59))),
+df_scenes = search_stac_scenes(
     geometry=geojson,
     date_start=date_start,
     date_end=date_end,
@@ -46,6 +45,6 @@ df_scenes_v0 = search_stac_scenes(
     query={"eo:cloud_cover": {"lt": 1}},
 )
 
-df_properties = parse_nested_stac_data(df=df_scenes_v0, column="properties")
-df_assets = parse_nested_stac_data(df=df_scenes_v0, column="assets")
-df_asset_info = request_asset_info(df=df_scenes_v0)
+df_properties = parse_nested_stac_data(df=df_scenes, column="properties")
+df_assets = parse_nested_stac_data(df=df_scenes, column="assets")
+df_asset_info = request_asset_info(df=df_scenes)
