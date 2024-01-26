0d2483b241979c0c815c1c7bdbb35d17f262c061
tnigon <8249296+tnigon@users.noreply.github.com>
2024-01-26 10:56:47 -0600

    filter_scenes_with_all_nodata to init

--- pixels_utils/scenes/__init__.py
+++ pixels_utils/scenes/__init__.py
@@ -1,3 +1,8 @@
-from pixels_utils.scenes._scenes import parse_nested_stac_data, request_asset_info, search_stac_scenes
+from pixels_utils.scenes._scenes import (
+    filter_scenes_with_all_nodata,
+    parse_nested_stac_data,
+    request_asset_info,
+    search_stac_scenes,
+)
 
-__all__ = ("parse_nested_stac_data", "request_asset_info", "search_stac_scenes")
+__all__ = ("filter_scenes_with_all_nodata", "parse_nested_stac_data", "request_asset_info", "search_stac_scenes")
